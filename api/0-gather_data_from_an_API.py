ff0627933a57de2c2b95624f4558e7e08b2b0a90
nalule-grace <g.nalule@alustudent.com>
2023-07-08 19:30:53 +0200

    alu-back-end

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+if len(sys.argv) < 2:
+    print("Please provide an employee ID")
+    sys.exit(1)
+
+employee_id = int(sys.argv[1])
+url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+    employee_id)
+
+response = requests.get(url)
+
+if response.status_code != 200:
+    print("Error retrieving TODO list")
+    sys.exit(1)
+
+todos = response.json()
+total_tasks = len(todos)
+completed_tasks = sum(1 for todo in todos if todo.get('completed'))
+
+employee_name = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                             .format(employee_id)).json().get('name')
+
+print("Employee {} is done with tasks({}/{}):".format(
+    employee_name, completed_tasks, total_tasks))
+
+for todo in todos:
+    if todo.get('completed'):
+        print("\t {}".format(todo.get('title')))
